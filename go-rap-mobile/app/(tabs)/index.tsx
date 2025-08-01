090a930efd8f2511e28cfbe7b31fcbf703c2f53f
ranadeep <ranadeep@digiworldexpo.com>
2025-08-01 11:35:35 +0530

    initial set up with succesfull compiling

---
+++ go-rap-mobile/app/(tabs)/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello Web</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#222',
+  },
+  text: {
+    fontSize: 22,
+    color: '#fff',
+  },
+});
