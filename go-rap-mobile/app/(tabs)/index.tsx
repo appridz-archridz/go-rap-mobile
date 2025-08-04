6b773e5c0615977585357f1b7d6277a5eef7fda8
ranadeep <ranadeep@digiworldexpo.com>
2025-08-04 10:08:18 +0530

    routing

--- go-rap-mobile/app/(tabs)/index.tsx
+++ go-rap-mobile/app/(tabs)/index.tsx
@@ -1,23 +1,10 @@
+
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Redirect } from 'expo-router';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello Web</Text>
-    </View>
+    <Redirect href={'/(tabs)'} />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#222',
-  },
-  text: {
-    fontSize: 22,
-    color: '#fff',
-  },
-});
